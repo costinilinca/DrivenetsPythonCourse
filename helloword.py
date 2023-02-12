2bd09be536a7a0966c4ae2e6ac8dc594485c7fff
costin.ilinca <cilinca@drivenets.com>
2023-02-13 00:43:25 +0200

    1st commit in git

---
+++ helloword.py
@@ -0,0 +1,9 @@
+# this is my 1st code
+
+print("Hello Word!", "I want this to be on the same line as the one above", sep="!!!")   # added a sep parameter
+print("Hello Word!", end="")
+print("I want this to be on the same line as the one above")
+print("Hello Word!", "I want this to be on the same line as the one above", sep="anything")
+# a="sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+# b="ccccccccc"
+# print(a,b, sep='')
